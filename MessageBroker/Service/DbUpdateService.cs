c5cc351e1dd0d49e8c590f3179b42b15e3ae62a1
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2019-05-18 07:27:16 +0700

    a

---
+++ MessageBroker/Service/DbUpdateService.cs
@@ -0,0 +1,47 @@
+﻿using CacheEngineShared;
+using Grpc.Core;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using MessageShared;
+using Grpc.Core.Utils;
+using System.Configuration;
+
+namespace MessageBroker
+{
+    public class mDbUpdateServiceImpl : mDbUpdateService.mDbUpdateServiceBase
+    {
+        private readonly IDataflowSubscribers _dataflow;
+        public mDbUpdateServiceImpl(IDataflowSubscribers dataflow) {
+            this._dataflow = dataflow;
+        }
+
+        public override Task<mDbUpdateReply> Send(mDbUpdateRequest request, ServerCallContext context)
+        {
+            _dataflow.enqueue(new JobDbUpdate(request)).Wait();
+            return Task.FromResult(new mDbUpdateReply() { });
+        }
+    }
+
+    public class DbUpdateSerive
+    {
+        static Server server;
+        public static void Start(IDataflowSubscribers dataflow)
+        {
+            string HOST_DB_UPDATE = ConfigurationManager.AppSettings["HOST_DB_UPDATE"];
+            string PORT_DB_UPDATE = ConfigurationManager.AppSettings["PORT_DB_UPDATE"];
+            server = new Server()
+            {
+                Services = { mCacheService.BindService(new mCacheFindServiceImpl(dataflow)) },
+                Ports = { new ServerPort(HOST_DB_UPDATE, int.Parse(PORT_DB_UPDATE), ServerCredentials.Insecure) }
+            };
+            server.Start();
+        }
+
+        public static void Stop()
+        {
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
---
+++ MessageShared/Grpc/DbUpdateProvider.cs
@@ -0,0 +1,43 @@
+﻿using Grpc.Core;
+using System;
+using System.Configuration;
+using System.Runtime.InteropServices;
+
+namespace MessageShared
+{ 
+    public static class DbUpdateProvider
+    {
+        public static mDbUpdateService.mDbUpdateServiceClient init()
+        {
+            try
+            {
+                string HOST_DB_UPDATE = ConfigurationManager.AppSettings["HOST_DB_UPDATE"];
+                string PORT_DB_UPDATE = ConfigurationManager.AppSettings["PORT_DB_UPDATE"];
+
+                Channel channel = new Channel(HOST_DB_UPDATE + ":" + PORT_DB_UPDATE, ChannelCredentials.Insecure);
+
+                var client = new mDbUpdateService.mDbUpdateServiceClient(channel);
+
+                //client.writeLogText(new mLogText() { Text = Guid.NewGuid().ToString() }); 
+                //channel.ShutdownAsync().Wait();
+
+                return client;
+            }
+            catch { }
+            return null;
+        }
+
+        public static mDbUpdateReply Find(this mDbUpdateService.mDbUpdateServiceClient cache, mDbUpdateRequest request)
+        {
+            try
+            {
+                if (cache != null)
+                {
+                   return cache.Send(request);
+                }
+            }
+            catch { }
+            return null;
+        }
+    }
+}
