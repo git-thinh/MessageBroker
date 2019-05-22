e5f0df3bdaa13bb1e8bbcfb334539e5905ea0c07
nguyenvanthinh <nguyenvanthinh@f88.vn>
2019-05-21 21:50:15 -0700

    a

---
+++ MessageBroker/Job/JobPdfExport.cs
@@ -0,0 +1,50 @@
+﻿using CacheEngineShared;
+using MessageShared;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace MessageBroker
+{
+    public class JobPdfExport : JobBase
+    {
+        public override void freeResource() { dbStop(); }
+
+        ////////////////////////////////////////////////////////////////////
+        /// 
+
+        private static bool _inited = false;
+        private readonly mDbUpdateRequest _requestUpdateDb;
+        public JobPdfExport(mDbUpdateRequest requestUpdateDb = null)
+        {
+            _requestUpdateDb = requestUpdateDb;
+        }
+
+        ////////////////////////////////////////////////////////////////////
+        /// 
+         
+        static void dbStart()
+        { 
+        }
+
+        static void dbStop() { 
+        }
+
+        ////////////////////////////////////////////////////////////////////
+        /// 
+
+        public override void execute()
+        {
+            if (!_inited)
+            {
+                _inited = true;
+                dbStart();
+                return;
+            }
+
+            if (_requestUpdateDb == null) return; 
+        }
+         
+    }
+}
