00ccfa79bb3e07944e65254415bcdd0cc93828b2
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2019-05-26 23:36:59 +0700

    a

---
+++ MessageUI/view/test/webview/js.js
@@ -0,0 +1,19 @@
+﻿var WEBVIEW_CONFIG = {
+    requiresAuth: false,
+    path: '/test/webview'
+};
+
+var WEBVIEW_COM = Vue.component('webview', {
+    mixins: [_MIXIN, _COMS],
+    data: function () {
+        var data = {
+            _name: 'webview'
+        };
+        return data;
+    },
+    template: _apiGet('view/test/webview/index.html'),
+    mounted: function () {
+        document.title = 'WEBVIEW';
+        $(".alert").alert('close');
+    }
+});
